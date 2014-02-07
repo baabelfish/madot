e2085b2ef23fab36fa9caad1c556eb0da6c4a66f
Niko Junkala <baabelfish@gmail.com>
2014-02-07 16:41:24 +0200

    ssh DISPLAY wat

--- src/madot/core.clj
+++ src/madot/core.clj
@@ -1,6 +1,18 @@
 (ns madot.core
-  (:require [quil.core :as q]))
+  (:require [quil.core :refer :all]))
 
-(defn -main
+(defn setup
   []
-  (println "Hello, World!"))
+  (smooth)
+  (frame-rate 10)
+  (background 0))
+
+(defn draw
+  []
+  (println "Drawing frame "))
+
+(defsketch madot
+  :title "madot"
+  :setup setup
+  :draw draw
+  :size 640 640)
